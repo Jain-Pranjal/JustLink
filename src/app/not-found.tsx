a7511ede9e9d632b8d70c64d8a7e1f8928e59e18
Pranjal Jain <pranjalworkon@gmail.com>
2025-08-21 18:10:16 +0530

    fix: 404 static images issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,19 @@
+import { GlobalNavbar } from '@/components/global/Navbar'
+import NotFoundClient from '@/components/NotFoundClient'
 
-import NotFoundClient from "@/components/NotFoundClient";
-
-import { Metadata } from "next";
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-    title: "Oops! Page Not Found",
+    title: 'Oops! Page Not Found',
     description:
         "Looks like you've taken a wrong turn! The page you're searching for seems to have vanished into the digital void. No worries though - let's navigate you back to our main content hub.",
-};
-
-
+}
 
 export default function NotFound() {
     return (
-        <NotFoundClient />
+        <>
+            <GlobalNavbar />
+            <NotFoundClient />
+        </>
     )
 }
