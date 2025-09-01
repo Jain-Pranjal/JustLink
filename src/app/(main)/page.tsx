6b528ac03ba00c26ea284dff190ee7e261400d52
Pranjal Jain <pranjalworkon@gmail.com>
2025-09-01 14:48:09 +0530

    fix: remove the fetching data from landing

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import { getQueryClient, trpc } from '@/trpc/server'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 import LandingPage from '@/modules/landing/ui/views/LandingPage'
 
 const page = async () => {
-    // fetching initial data for the waitlist count
-    const queryClient = getQueryClient()
-    void queryClient.prefetchQuery(
-        trpc.waitlist.getTotalWaitlistEntries.queryOptions()
-    )
-
-    return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <LandingPage />
-        </HydrationBoundary>
-    )
+    return <LandingPage />
 }
 
 export default page
